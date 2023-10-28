07c25a5d12783a938665a888d55c26ee7448234c
marieham45 <marie.hamsikova@centrum.cz>
2023-10-28 18:56:48 +0200

    Header Component + styles added

---
+++ src/Components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div className="header">
+            <h1>The Great Memory Game</h1>
+            <span className="countdown hidden"></span>
+        </div>
+    );
+};
+
+export default Header;
