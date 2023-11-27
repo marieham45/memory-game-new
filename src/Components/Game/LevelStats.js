d9bddcbbdcf75a11837d645871416080230353fa
marieham45 <marie.hamsikova@centrum.cz>
2023-11-27 23:09:30 +0100

    percentage computation fixed

--- src/Components/Game/LevelStats.js
+++ src/Components/Game/LevelStats.js
@@ -1,16 +1,17 @@
 import React from 'react';
 
 const LevelStats = ({level, correctCount, attempts}) => {
+    console.log({level, correctCount, attempts})
     return (
         <>
             <p className="stats_level">Level {level}</p>
             {correctCount ? <>
                 <div className="column-graph">
 
-                    {Object.entries(correctCount).map(([label, value], index) => (
+                    {Object.values(correctCount).map((value, index) => (
                         <div key={index} className={`column ${level === "Beginner" ? "green_level" : level === "Advanced" ? "orange_level" : "blue_level"}`}
-                             style={{height: `${Math.round(value / attempts) * 75 + 26}%`}}>
-                            <span>{Math.round(value / attempts) * 100}%<br/>({value})</span>
+                             style={{height: `${Math.round(value / attempts * 75) + 26}%`}}>
+                            <span>{Math.round(value / attempts * 100)}%<br/>({value})</span>
                         </div>
                     ))}
 
