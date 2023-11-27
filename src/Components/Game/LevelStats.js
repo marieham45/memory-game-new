9390348d4097583623f4f8698b107944f21a6ee0
marieham45 <marie.hamsikova@centrum.cz>
2023-11-27 07:23:20 +0100

    new LevelStats component, styles optimised

---
+++ src/Components/Game/LevelStats.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+const LevelStats = ({level, correctCount, attempts}) => {
+    return (
+        <>
+            <p className="stats_level">Level {level}</p>
+            {correctCount ? <>
+                <div className="column-graph">
+
+                    {Object.entries(correctCount).map(([label, value], index) => (
+                        <div key={index} className={`column ${level === "Beginner" ? "green_level" : level === "Advanced" ? "orange_level" : "blue_level"}`}
+                             style={{height: `${Math.round(value / attempts) * 75 + 26}%`}}>
+                            <span>{Math.round(value / attempts) * 100}%<br/>({value})</span>
+                        </div>
+                    ))}
+
+                </div>
+                <div className="column-label">
+                    {Object.entries(correctCount).map((_, index) => (
+                            <p key={index}>{index}</p>
+                        )
+                    )}
+                </div>
+            </> : <p className="no_stats">No stats yet...</p>}
+
+
+        </>
+    );
+};
+
+export default LevelStats;
--- src/Components/Game/Stats.js
+++ src/Components/Game/Stats.js
@@ -1,3 +1,5 @@
+import LevelStats from "./LevelStats";
+import React from "react";
 
 const Stats = ({setShowStats}) => {
     const beginnerCorrectCount = JSON.parse(localStorage.getItem('beginnerCorrectCount')) || ""
@@ -12,60 +14,11 @@ const Stats = ({setShowStats}) => {
     return (
         <div className='stats'>
             <button className="stats_button" onClick={() => setShowStats(false)}>✖</button>
-            <p className="stats_level">Level Beginner</p>
-            {beginnerCorrectCount ? <>
-                <div className="column-graph">
-
-                    {Object.entries(beginnerCorrectCount).map(([label, value], index) => (
-                        <div key={index} className="column green_level"
-                             style={{height: `${Math.round(value / beginnerAttempts) * 75 + 26}%`}}>
-                            <span>{Math.round(value / beginnerAttempts) * 100}%<br/>({value})</span>
-                        </div>
-                    ))}
-
-                </div>
-                <div className="column-label">
-                    {Object.entries(beginnerCorrectCount).map((_, index) => (
-                            <p key={index}>{index}</p>
-                        )
-                    )}
-                </div>
-            </> : <p style={{height: "21%"}}>No stats yet...</p>}
+            <LevelStats level="Beginner" correctCount={beginnerCorrectCount} attempts={beginnerAttempts}/>
             <hr/>
-            <p className="stats_level">Level Advanced</p>
-            {advancedCorrectCount ? <> <div className="column-graph">
-
-                {Object.entries(advancedCorrectCount).map(([label, value], index) => (
-                    <div key={index} className="column orange_level"
-                         style={{height: `${Math.round(value / advancedAttempts) * 75 + 26}%`}}>
-                        <span>{Math.round(value / advancedAttempts) * 100}%<br/>({value})</span>
-                    </div>
-                ))}
-
-            </div>
-             <div className="column-label">
-                {Object.entries(advancedCorrectCount).map((_, index) => (
-                    <p key={index}>{index}</p>
-                ))}
-            </div>
-            </> : <p style={{height: "21%"}}>No stats yet...</p>}<hr/>
-            <p className="stats_level">Level Hardcore</p>
-            {hardcoreCorrectCount ? <> <div className="column-graph">
-
-                {Object.entries(hardcoreCorrectCount).map(([label, value], index) => (
-                    <div key={index} className="column blue_level"
-                         style={{height: `${Math.round(value / hardcoreAttempts) * 75 + 26}%`}}>
-                        <span>{Math.round(value / hardcoreAttempts) * 100}%<br/>({value})</span>
-                    </div>
-                ))}
-
-            </div>
-            <div className="column-label">
-                {Object.entries(hardcoreCorrectCount).map((_, index) => (
-                    <p key={index}>{index}</p>
-                ))}
-            </div>
-            </> : <p style={{height: "21%"}}>No stats yet...</p>}
+            <LevelStats level="Advanced" correctCount={advancedCorrectCount} attempts={advancedAttempts}/>
+            <hr/>
+            <LevelStats level="Hardcore" correctCount={hardcoreCorrectCount} attempts={hardcoreAttempts}/>
         </div>
 
     );
