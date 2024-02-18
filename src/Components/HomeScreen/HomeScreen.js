5fb595c776fb1c9bec2e3f1492b2c2c7ae313ae6
marieham45 <marie.hamsikova@centrum.cz>
2024-02-18 15:23:23 +0100

    adding stats button to home screen

--- src/Components/HomeScreen/HomeScreen.js
+++ src/Components/HomeScreen/HomeScreen.js
@@ -1,9 +1,10 @@
 import Title from "./Title";
 import Penguin from "./Penguin";
 import Rules from "./Rules";
-import {useState} from "react";
+import React, {useState} from "react";
 import Levels from "./Levels";
 import HomeScreenButton from "../Buttons/HomeScreenButton";
+import Stats from "../Game/Stats";
 
 const HomeScreen = ({
                         setLimitForRemembering, limitForRemembering, setLimitForPlayerInput, limitForPlayerInput,
@@ -11,6 +12,7 @@ const HomeScreen = ({
                     }) => {
     const [showRules, setShowRules] = useState(false)
     const [showLevels, setShowLevels] = useState(onReturn)
+    const [showStats, setShowStats] = useState(false)
 
     const handleGoToGame = () => {
         setShowLevels(true)
@@ -19,21 +21,24 @@ const HomeScreen = ({
 
     return (
         <div className=".home_screen">
-            {!showRules && !showLevels &&
+            {!showStats && !showRules && !showLevels &&
                 <>
                     <Title/>
                     <Penguin/>
                     <HomeScreenButton onClick={() => setShowRules(true)}>What are <br/> the rules?</HomeScreenButton>
                     <HomeScreenButton onClick={handleGoToGame}>Take me to <br/> the game!</HomeScreenButton>
+                    <HomeScreenButton onClick={() => setShowStats(true)}>Show my <br/>stats</HomeScreenButton>
                 </>
             }
-            {!showRules && showLevels &&
+            {!showStats && !showRules && showLevels &&
                 <Levels setLimitForRemembering={setLimitForRemembering} limitForRemembering={limitForRemembering}
                         setLimitForPlayerInput={setLimitForPlayerInput} limitForPlayerInput={limitForPlayerInput}
                         onReset={onReset} onGameStart={onGameStart} setShowRules={setShowRules} setShowLevels={setShowLevels}
                 />
             }
-            {showRules && <Rules onGoToGame={handleGoToGame}/>}
+            {!showStats && showRules && <Rules onGoToGame={handleGoToGame}/>}
+            {showStats && <Stats setShowStats={setShowStats}/>}
+
         </div>
     );
 };
